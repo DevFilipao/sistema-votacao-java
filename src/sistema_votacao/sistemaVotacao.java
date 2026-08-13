0a927b892799d8cc8c373ce805f7dba7796b05fa
Filipe Emanuel Rodrigues Nunes <filipe.11796376@aluno.mg.gov.br>
2026-08-13 10:31:37 -0300

    chore: criar estrutura inicial do programa

--- src/sistema_votacao/sistemaVotacao.java
+++ src/sistema_votacao/sistemaVotacao.java
@@ -1,4 +1,15 @@
 package sistema_votacao;
 
+import java.util.Scanner;
+
 public class sistemaVotacao {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main (String[] args){
+        System.out.println("Sistema de votação iniciado.");
+        scanner.close();
+
+
+    }
 }
