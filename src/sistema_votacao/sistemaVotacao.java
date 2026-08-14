ade29f77acb18fb2dde31958217d76beb92e1ac3
Filipe Emanuel Rodrigues Nunes <filipe.11796376@aluno.mg.gov.br>
2026-08-14 09:33:06 -0300

    implementar menu principal com do-while

--- src/sistema_votacao/sistemaVotacao.java
+++ src/sistema_votacao/sistemaVotacao.java
@@ -6,7 +6,7 @@ public class sistemaVotacao {
 
     static Scanner scanner = new Scanner(System.in);
 
-    public static void main (String[] args){
+    public static void main (String[] args) {
         System.out.println("Sistema de votação iniciado.");
         scanner.close();
 
@@ -14,7 +14,7 @@ public class sistemaVotacao {
         final int TOTAL_TURMAS = 3;
         final int MAX_VOTANTES_POR_TURMA = 10;
 
-        int [] numeroCandidatos = new int[MAX_CANDIDATOS];
+        int[] numeroCandidatos = new int[MAX_CANDIDATOS];
         String[] nomesCandidatos = new String[MAX_CANDIDATOS];
         int[] votosCandidatos = new int[MAX_CANDIDATOS];
 
@@ -22,6 +22,40 @@ public class sistemaVotacao {
 
         int[] quantidadeVotosTurma = new int[TOTAL_TURMAS];
         int quantidadeCandidatos = 0;
+
+        int opcao;
+
+        do {
+            System.out.println("\n===== SISTEMA DE VOTAÇÃO =====");
+            System.out.println("1 - Cadastrar candidatos");
+            System.out.println("2 - Iniciar votação");
+            System.out.println("3 - Exibir resultado");
+            System.out.println("4 - Exibir matriz de votos");
+            System.out.println("5 - Sair");
+
+            System.out.print("Opção: ");
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Cadastro selecionado.");
+                    break;
+                case 2:
+                    System.out.println("Votação selecionada.");
+                    break;
+                case 3:
+                    System.out.println("Resultado selecionado.");
+                    break;
+                case 4:
+                    System.out.println("Matriz selecionada.");
+                    break;
+                case 5:
+                    System.out.println("Sistema encerrado.");
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+            }
+        } while (opcao != 5);
 
 
     }
